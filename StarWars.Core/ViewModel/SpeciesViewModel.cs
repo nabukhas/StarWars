90b909cb55d7cf8a518cb267213b5ac7da28e2c3
Naser Abu Khas <nabukhas@gmail.com>
2019-12-22 14:17:53 +0400

    missing file

---
+++ StarWars.Core/ViewModel/SpeciesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarWars.Core.ViewModel
+{
+    public class SpeciesViewModel
+    {
+        public int SpeciesId { get; set; }
+        public string SpeciesName { get; set; }
+        public int SpeciesCharacterCount { get; set; }
+    }
+}
